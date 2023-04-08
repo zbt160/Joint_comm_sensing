48a45131a7d2da8a3c30b63a6ebf8495934c3eaa
zbt160 <zaidtariq4@gmail.com>
2023-04-08 16:14:36 -0500

    Computing Wa, R_t, Omega
    This calculates the Wa Rw, R_t and Omega

---
+++ get_Rw_over_sigma.m
@@ -0,0 +1,15 @@
+function [Rw] = get_Rw_over_sigma(All_A,wa,SNR_ratio)
+%     DISP("fidnig Rt");
+    len = length(All_A);
+    ttH = wa*wa';
+    for i=1:len
+        
+        A_thetaM = All_A{i};
+        A_hermM = A_thetaM.';
+        if i==1
+            Rw = SNR_ratio*A_hermM*ttH*A_thetaM;
+        else
+            Rw = Rw+ SNR_ratio*A_hermM*ttH*A_thetaM;
+        end
+    end
+end
